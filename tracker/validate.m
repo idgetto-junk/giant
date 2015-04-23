2b721e63478a5b360e197f7723c1dd05505209b7
unknown <tdye@2VMFG12.olin.edu>
2015-04-23 12:30:23 -0400

    Implement tracker data in MATLAB

---
+++ tracker/validate.m
@@ -0,0 +1,22 @@
+function res = validate
+    clf;
+    hold on;
+    load knee2.csv;
+    load torso2.csv;
+    T = knee2(:, 1);
+    kneeX = knee2(:, 2);
+    kneeY = knee2(:, 3);
+    torsoX = torso2(:, 2);
+    torsoY = torso2(:, 3);
+    for i = 1:length(T)
+        Theta1(i) = atan(torsoY(i)/torsoX(i));
+        Theta2(i) = atan((kneeY(i)-torsoY(i))/(kneeX(i)-torsoX(i)));
+        Amplitude(i) = Theta2(i)-Theta1(i);
+    end
+%     plot(Theta1, 'r');
+%     plot(Theta2,  'g');
+%     plot(Amplitude, 'b');
+    plot(Amplitude);
+    period = 3.04-0.968;
+    disp(period);
+end
