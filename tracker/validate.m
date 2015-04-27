125b5b1ffaa54e5a5f6cae8e271278741a009079
unknown <tdye@2VMFG12.olin.edu>
2015-04-26 23:09:50 -0400

    Add necessary figures for TP4 and motify tracker validation

--- tracker/validate.m
+++ tracker/validate.m
@@ -1,4 +1,5 @@
 function res = validate
+    % TURN THIS INTO AMPLITUDE VS. THETA AND TOTAL ENERGY VS. THETA
     clf;
     hold on;
     load knee2.csv;
@@ -9,14 +10,14 @@ function res = validate
     torsoX = torso2(:, 2);
     torsoY = torso2(:, 3);
     for i = 1:length(T)
-        Theta1(i) = atan(torsoY(i)/torsoX(i));
-        Theta2(i) = atan((kneeY(i)-torsoY(i))/(kneeX(i)-torsoX(i)));
+        Theta1(i) = atan2(torsoY(i),torsoX(i));
+        Theta2(i) = atan2((kneeY(i)-torsoY(i)),(kneeX(i)-torsoX(i)));
         Amplitude(i) = Theta2(i)-Theta1(i);
     end
-%     plot(Theta1, 'r');
-%     plot(Theta2,  'g');
-%     plot(Amplitude, 'b');
-    plot(Amplitude);
+    %plot(Theta1, 'r');
+    %plot(Theta2,  'g');
+    plot(Amplitude, 'b');
+  %  plot(Amplitude);
     period = 3.04-0.968;
     disp(period);
 end
