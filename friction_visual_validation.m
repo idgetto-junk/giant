17dbe4b4d439d1cf3a2d4bdb7bfeec2284f68973
unknown <tdye@2VMFG12.olin.edu>
2015-05-01 14:14:08 -0400

    make some final figures

--- friction_visual_validation.m
+++ friction_visual_validation.m
@@ -9,8 +9,8 @@ function friction_visual_validation
     theta_real = Y1_real(:, 1);
     theta_model = Y_model(:, 1);
     
-    plot(T_real, theta_real, 'r');
-    plot(T_model+.9, theta_model, 'g');
+    plot(T_real, theta_real, 'r', 'LineWidth', 3);
+    plot(T_model+.9, theta_model, 'g', 'LineWidth', 3);
     
     xlabel('Time (sec)');
     ylabel('\theta_1 (rad)');
