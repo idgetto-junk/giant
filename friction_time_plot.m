085b466f7ff53429986b3120d144db93da17381e
unknown <tdye@2VMFG12.olin.edu>
2015-04-30 21:53:04 -0400

    Implement friction validation using rotational period

---
+++ friction_time_plot.m
@@ -0,0 +1,26 @@
+function friction_time_plot
+    clf;
+    hold on;
+    
+    F = linspace(0, 1.5, 100);
+    for index = 1:length(F)
+        index
+        [T, Y] = giant_ode_friction(F(index));
+        
+        if Y(end, 1) >= pi
+            time = T(end);
+        else
+            time = 0;
+        end
+
+        Time(index) = time;
+    end
+
+    clf;
+    plot(F, smooth(Time, 10), 'LineWidth', 4);
+    %plot(2.169);
+    
+    title('Coefficient of Friction vs. Rotational Period');
+    xlabel('Coefficient of Friction F/v^2');
+    ylabel('Rotational Period');
+end
---
+++ giant_ode_friction.m
@@ -0,0 +1,17 @@
+function [T, Y] = giant_ode_friction(uk)
+% GIANT_ODE_AMP - calculates the angle and angular velocity of a double pendulum over time with a given amplitude of leg swing
+%
+% Inputs:
+%   amplitude - the amplitude of the gymnasts leg swing
+%
+% Outputs:
+%    T - a vector of times
+%    Y - a matrix of thetas and omegas
+%
+% Author: Isaac Getto
+% email address: isaac.getto@students.olin.edu
+% April 2015
+
+    [T, Y] = giant_ode(containers.Map({'uk'}, {uk}));
+
+end
